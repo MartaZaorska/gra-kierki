9190ceae9f8a7aa0580da008a4b7393cceaa8efe
Marta Zaorska <marta.zaorska2@gmail.com>
2023-05-23 18:58:57 +0200

    Updated style

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ let selectedCards = [];
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(() => {
+    navigator.serviceWorker.register('service-worker.js').then(() => {
       console.log('SW registered');
     }).catch(registrationError => {
       console.log('SW registration failed', registrationError);
